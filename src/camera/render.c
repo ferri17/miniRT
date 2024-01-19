ad308a9657bca6b20c45379d7cec37d30ed1a898
Ferran Bosch <fbosch@car9s1.42barcelona.com>
2024-01-19 21:12:42 +0100

    reflection test

--- src/camera/render.c
+++ src/camera/render.c
@@ -6,11 +6,14 @@
 /*   By: fbosch <fbosch@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:13:17 by fbosch            #+#    #+#             */
-/*   Updated: 2024/01/19 17:13:09 by fbosch           ###   ########.fr       */
+/*   Updated: 2024/01/19 21:11:35 by fbosch           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MiniRT.h"
+
+t_color	render_raytrace_mode_reflect(t_scene *scene, const t_ray *r, t_world *hit_obj,
+		t_hit *hit_rec);
 
 void	draw_selection_mask(const t_ray *r, t_scene *scene, t_world *objs,
 		int i, int j)
@@ -50,6 +53,12 @@ t_color	send_ray(const t_ray *r, t_scene *scene, int i, int j)
 	}
 	if (hit_obj)
 	{
+		/*
+		*/
+		if (hit_obj == scene->objs)
+			return (render_raytrace_mode_reflect(scene, r, hit_obj, &hit_rec));
+		/*
+		*/
 		if (scene->render_mode == EDIT_MODE)
 			return (render_edit_mode(scene, hit_obj, r, &hit_rec));
 		else
@@ -114,3 +123,71 @@ t_color	render_raytrace_mode(t_scene *scene, const t_ray *r, t_world *hit_obj,
 	}
 	return (pxl_color);
 }
+
+t_color	render_raytrace_mode_reflect(t_scene *scene, const t_ray *r, t_world *hit_obj,
+		t_hit *hit_rec)
+{
+	t_color	albedo;
+	t_color	diffuse_light;
+	t_color	specular_light;
+	t_ray	r_light;
+	t_light	*lights;
+
+	albedo = hit_obj->get_color(&hit_rec->p, hit_obj);
+	albedo = calc_ambient_light(&scene->amblight.color, &albedo,
+			scene->amblight.ratio);
+	lights = scene->light;
+	while (lights)
+	{
+		calc_shadow_ray(&r_light, lights, hit_rec);
+		if (calc_hard_shadows(scene->objs, &r_light, hit_rec) == false)
+		{
+			diffuse_light = calc_diffuse_light(lights, &r_light, hit_rec,
+					hit_obj);
+			specular_light = calc_specular_light(lights, r, &r_light, hit_rec);
+			albedo = add_vec3(&albedo, &diffuse_light);
+			albedo = add_vec3(&albedo, &specular_light);
+		}
+		lights = lights->next;
+	}
+	albedo = product_vec3_r(&albedo, 0.5);
+	
+	
+	t_color	reflection = {0, 0, 0};
+
+	t_ray	reflected;
+
+	reflected.orig = product_vec3_r(&hit_rec->normal, BIAS);
+	reflected.orig = add_vec3(&hit_rec->p, &reflected.orig);
+	
+	t_vec3 dir = unit_vector(&r->dir);
+	reflected.dir = product_vec3_r(&hit_rec->normal, 2 * dot(&dir, &hit_rec->normal));
+	reflected.dir = substract_vec3(&dir, &reflected.dir);
+	
+	t_world *objs = scene->objs;
+	t_world	*hit_obj2;
+	t_hit	hit_rec2;
+
+	hit_rec2.ray_tmin = BIAS;
+	hit_rec2.ray_tmax = INT_MAX;
+	hit_obj2 = NULL;
+	while (objs)
+	{
+		if (hit_obj->hit(&reflected, objs->type, &hit_rec2))
+		{
+			hit_rec2.ray_tmax = hit_rec2.t;
+			hit_obj2 = objs;
+		}
+		objs = objs->next;
+	}
+	if (hit_obj2)
+		reflection = hit_obj2->get_color(&hit_rec2.p, hit_obj2);
+	reflection = product_vec3_r(&reflection, 0.1);
+
+	t_color tmp = add_vec3(&albedo, &reflection);
+	return (tmp);
+}
+
+/* vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
+} */
